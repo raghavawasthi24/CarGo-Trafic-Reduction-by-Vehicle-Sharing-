f7a7434491862b1b0df0f9d5c76cd87649e68d0b
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-01-02 12:50:29 +0530

    logout added

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,24 @@
 import React ,{useRef, useState} from 'react';
 import "./Navbar.css";
 import { Link } from 'react-router-dom';
+import profile from "./images/profile.png";
 import logo from "./images/logo.png";
 import Arrow from "./images/arrow.png";
 
 const Navbar = () => {
 
-  
+  const [openarrow,setopenarrow]=useState(false);
+
+  const arrow=()=>{
+    if(openarrow==false){
+    setopenarrow(true);
+  }
+  else{
+    setopenarrow(false)
+  }
+}
+
+
   return (
     <div className="navbar">
         {/* <img src={logo}/> */}
@@ -21,6 +33,13 @@ const Navbar = () => {
           {/* <Link to="/FAQS" className='link'>FAQS</Link> */}
           <Link to="/contact" className='link'>Contact Us</Link>
           <Link to="/offerARide" id="offer">Offer A Ride</Link>
+          <div className="profile" onClick={arrow}>
+            <img src={profile}/>
+          </div>
+          <div className={openarrow?"logout":"hide"}>
+            <p>View Profile</p>
+            <p>LogOut</p>
+          </div>
         </nav> 
     </div>
   )
